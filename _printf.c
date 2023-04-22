07e087964951c10a410ed9fd2d35b671e49701a0
root <sokayna.bennasseur@gmail.com>
2023-04-22 08:10:50 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -4,55 +4,54 @@
 #include <unistd.h>
 
 /**
- * _printf - Produces output 
+ * _printf - Produces output
  * @format: A character string
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int length;
-	int C, l;
+	int length, C, l;
 	char *s;
-	
+
 	va_start(args, format);
 	length = 0;
 	while (*format)
 	{
 		if (*format == '%')
-			{
-				format++;
-				if (*format == 'c')
-				{
-					C = va_arg(args, int);
-					write(1, &C, 1);
-					length++;
-				}
-				else if (*format == 's')
-				{
-					s = va_arg(args, char *);
-					l = strlen(s);
-					write(1, s, l);
-					length = length + l;
-				}
-				else if (*format == '%')
-				{
-					write(1, format, 1);
-					length++;
-				}
-				else
-				{
-					write(1, format - 1, 2);
-					length = length + 2;
-				}
-			}
-		else
 		{
-			write(1, format, 1);
-			length++;
+			format++;
+			if (*format == 'c')
+			{
+				C = va_arg(args, int);
+				write(1, &C, 1);
+				length++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				l = strlen(s);
+				write(1, s, l);
+				length = length + l;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				length++;
+			}
+			else
+			{
+				write(1, format - 1, 2);
+				length = length + 2;
+			}
 		}
-		format++;
+	else
+	{
+		write(1, format, 1);
+		length++;
 	}
-	va_end(args);
-	return (length);
+	format++;
+}
+va_end(args);
+return (length);
 }
