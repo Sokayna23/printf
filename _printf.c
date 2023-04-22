ed6fa89f8b48d03eb33ccfc7d9fcbbe8d656d397
root <sokayna.bennasseur@gmail.com>
2023-04-22 00:07:44 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _printf - Produces output 
+ * @format: A character string
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int length;
+	int C, l;
+	char *s;
+	
+	va_start(args, format);
+	length = 0;
+	while (*format)
+	{
+		if (*format == '%')
+			{
+				format++;
+				if (*format == 'c')
+				{
+					C = va_arg(args, int);
+					write(1, &C, 1);
+					length++;
+				}
+				else if (*format == 's')
+				{
+					s = va_arg(args, char *);
+					l = strlen(s);
+					write(1, s, l);
+					length = length + l;
+				}
+				else if (*format == '%')
+				{
+					write(1, format, 1);
+					length++;
+				}
+				else
+				{
+					write(1, format - 1, 2);
+					length = length + 2;
+				}
+			}
+		else
+		{
+			write(1, format, 1);
+			length++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (length);
+}
