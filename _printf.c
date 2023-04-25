82e91d8b253296aa10c9eafef0ecf36843ead106
root <sokayna.bennasseur@gmail.com>
2023-04-24 22:44:13 -0700

    _printf updated

--- _printf.c
+++ _printf.c
@@ -32,11 +32,7 @@ int _printf(const char *format, ...)
 			else if (_isinstr("buoxX", *format))
 				length += _puts(_uitoa_base(va_arg(args, int), *format));
 			else if (*format == 'p')
-			{
-				_putchar('0');
-				_putchar('x');
 				length += _puts(_uitoa_base(va_arg(args, unsigned int), *format));
-			}
 			else if (*format == '%')
 				length += _putchar(*format);
 			else
