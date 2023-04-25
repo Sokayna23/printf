f809364b646dec5c76d27bfa97a87c3edc16ac35
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-25 12:31:02 +0000

    Fixing bug on _putBase

--- _putbase.c
+++ _putbase.c
@@ -3,27 +3,28 @@
  * _putbase- _puts in base
  * @n: nb to convert and put
  * @flags : flags
- * @base : new base
+ * @toBase : new base
  * Return: nb char printed.
  */
-int _putbase(unsigned long int n, char base, t_flag flags)
+int _putbase(unsigned long int n, char toBase, t_flag flags)
 {
 	char *result;
 	int len = 0;
+	int base = 10;
 
-	if (base == 'b')
+	if (toBase == 'b')
 		base = 2;
-	else if (base == 'u')
+	else if (toBase == 'u')
 		base = 10;
-	else if (base == 'o')
+	else if (toBase == 'o')
 		base = 8;
-	else if (base == 'x' || base == 'X')
+	else if (toBase == 'x' || toBase == 'X')
 		base = 16;
 	result = _uitoa_base(n, base);
-	if (base == 'X')
+	if (toBase == 'X')
 		_to_upper(result);
 	if (flags.sharp)
-		len += _puts(base == 'X' ? "0X" : "0x");
+		len += _puts(toBase == 'X' ? "0X" : "0x");
 	len += _puts(result);
 	free(result);
 	return (len);
--- main.c
+++ main.c
@@ -11,11 +11,9 @@ int main(void)
 {
 	int _len;
 	int len;
-	int *p;
 
-	p = &_len;
-	_len = _printf("%p\n", p);
-	len = printf("%p\n", p);
+	_len = _printf("%+ + #X\n", 12);
+	len = printf("%+ + #X\n", 12);
 
 	printf("len = %d, _len = %d\n", len, _len);
 	return (0);
