8f28079e97d75b4ed4ba6e4652c0953cd8de15e4
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-25 22:03:18 +0000

    Fixing %p : adding flag + spc

--- _putadress.c
+++ _putadress.c
@@ -14,6 +14,12 @@ int _putAddress(unsigned long int address, t_flag *flags)
 	if (address == 0)
 		return (_puts("(nil)"));
 	flags->sharp = true;
+	if (flags->plus)
+		len += _putchar('+');
+	else if (flags->spc)
+		len += _putchar(' ');
+	flags->plus = false;
+	flags->spc = false;
 	len = _putbase(address, 'x', *flags);
 	return (len);
 }
