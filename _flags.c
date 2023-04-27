07cb8178e429ccd88372d7ea9dedf8798f1e3996
Sokayna24 <sokayna.bennasseur@gmail.com>
2023-04-27 02:29:46 +0000

    AutoPush

--- _flags.c
+++ _flags.c
@@ -47,7 +47,7 @@ void getFlags(const char **frm, t_flag *flags)
 		while (**frm && _isinstr("123456789", **frm))
 			(*frm)++;
 	}
-	if (_isinstr("lh", **frm) && _isinstr("diuoxX", *(*frm + 1)))
+	if (**frm && _isinstr("lh", **frm) && _isinstr("diuoxX", *(*frm + 1)))
 	{
 		flags->lenMod = **frm;
 		(*frm)++;
