617bbcd8b64eaf79b12faf44a4f7115ce4828272
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-28 02:01:14 +0000

    Fixing NULL _puts

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ int main(void)
 	int len;
 	int _len;
 
-    len = printf("%s\n", "test tetez"); 
-	_len = _printf("%s\n", "test tetez");
+    len = printf("%b", UINT_MAX); 
+	_len = _printf("%b", 45);
 	printf(" len : %d\n_len : %d\n", len, _len);
 	return (0);
 }
