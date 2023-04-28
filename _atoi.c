8fcbe4eeb91a876e76e3efcf174c9ac805144e54
SamirOuarrak <samir.ouarrak@gmail.com>
2023-04-28 16:46:21 +0000

    Update _atoi

--- _atoi.c
+++ _atoi.c
@@ -6,25 +6,12 @@
  */
 int _atoi(const char *str)
 {
-	int i;
 	unsigned int nb;
-	int neg;
 
-	i = 0;
-	neg = 1;
 	nb = 0;
-	while (str && str[i] && (str[i] == '0'))
-		i++;
-	if (!_isinstr("-0123456789", str[i]))
-		return (0);
-	while (str && str[i] && (str[i] < '0' || str[i] > '9'))
-		neg = str[i++] == '-' ? -neg : neg;
-	while (str && str[i] == '0')
-		i++;
-	while (str && str[i] && str[i] >= '0' && str[i] <= '9')
-	{
-		nb = (nb * 10) + ((int)str[i] - '0');
-		i++;
-	}
+	while (str && *str == '0')
+		str++;
+	while (str && *str >= '0' && *str <= '9')
+		nb = (nb * 10) + ((int)(*str++) - '0');
 	return (nb);
 }
